00c5691dade0f45ab9aecca8e6e2f7d6fac9d55b
manoj yadav <manojyadav@MacBookAir.mshome.net>
2026-03-28 12:11:43 +0530

    updated my project code

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const { createTodo } = require("./type");
 const app = express();
 
 app.use(express.json());
 
 app.post("/todo", function(req, res) {
+const createPayload=req.body;
+const parsePayload=createTodo.safeParse(createPayload);
+if(!parsePayload.sucees){
+    res.status(411).json({
+        msg:"You sent wrong input",
+    })
+    return;
+}
 
 })
 
@@ -12,5 +21,14 @@ app.get("/todos", function(req, res) {
 })
 
 app.put("/completed", function(req, res) {
+
+    const createPayload=req.body;
+    const parsePayload=createTodo.safeParse(createPayload);
+    if(!parsePayload.sucees){
+        res.status(411).json({
+            msg:"You sent wrong input",
+        })
+        return;
+    }
 
 }) 
